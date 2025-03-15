48023cebd66c145ed5255928635c835a7206c04c
adolfiscariot <m10mathenge@gmail.com>
2025-03-15 12:05:37 +0300

    added delete tasks feature

--- to-do.c
+++ to-do.c
@@ -54,34 +54,93 @@ void add_task() {
 void show_tasks(){
 	//open file
 	FILE *fp = fopen("tasks.txt", "r");
+
 	//if fille is null throw error
 	if(fp == NULL){
 		perror("Couldn't open file");
 		return;
 	}
+
 	//create buffer to store fetched tasks
 	char *fetched_tasks = calloc(TOTAL_TASKS, TASK_LENGTH);
+
 	//while fgets != NULL print tasks
+	printf("-----TASK MANAGER-----\n");
 	while ((fgets(fetched_tasks, TASK_LENGTH, fp)) != NULL)
 		printf("%d: %s\n", ++task_counter, fetched_tasks);
+
 	//free memory and close file
 	free(fetched_tasks);
 	fclose(fp);
 }
+
+//add function to delete task
+void delete_task(){
+
+	//ask user which idea they want to delete
+	printf("Which idea do you want to delete?...");
+	
+	//open tasks.txt (check for error)
+	FILE *fp = fopen("tasks.txt", "r");
+	if (fp == NULL){
+		perror("Couldn't open file");
+		return;
+	}
+	
+	//open temp.txt
+	FILE *fp2 = fopen("temp.txt", "w");
+	if (fp2 == NULL){
+		perror("Couldn't open file");
+		return;
+	}
+	
+	//get user's choice
+	int user_choice;
+	scanf("%d", &user_choice);
+	
+	//initialize line counter and task buffer
+	int line_counter = 0;
+	char task_buffer[TASK_LENGTH];
+
+	//read from tasks.txt
+	while ((fgets(task_buffer, TASK_LENGTH, fp)) != NULL){
+		//add line counter
+		line_counter++;
+		if (line_counter != user_choice)
+			fputs(task_buffer, fp2);
+	}
+	//close the files
+	fclose(fp2);
+	fclose(fp);
+
+	//delete tasks.txt and eename tempt.txt to tasks.txt
+	remove("tasks.txt");
+	rename("temp.txt", "tasks.txt");
+
+	//print task deleted
+	printf("Task %d deleted!\n", user_choice);
+	return;
+}
+
 int main(void){
 	//tell user welcome to task manager and select a choice
 	printf("WELCOME TO THE TASK MANAGER!\n");
 	printf("Please select a choice.Press...\n");
-	printf("1 to show tasks\n2 to add a task\n");
+	printf("1 to show tasks\n2 to add a task\n3 to delete task\n");
+
 	//create buffer to store user input
 	char user_input[4];
+
 	//get user input and store it in buffer
 	fgets(user_input, sizeof(int), stdin);
+
 	//convert string user input to an int
 	int converted_user_input = atoi(user_input);
+
 	//switch case for which choice they select
 	switch(converted_user_input){
 		case 1: show_tasks(); break;
 		case 2: add_task(); break;
+		case 3: delete_task(); break;
 	}
 }
